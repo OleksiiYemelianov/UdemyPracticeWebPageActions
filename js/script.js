cdebe58f41b223c564f56ea2fc2d52d059059b66
Oleksii <nebezotveta@gmail.com>
2021-08-21 09:26:12 +0300

    adding and removing element, practice, lesson 30

--- js/script.js
+++ js/script.js
@@ -23,4 +23,61 @@ const movieDB = {
         "Скотт Пилигрим против..."
     ]
 };
+// МОЙ КОД, КОТОРЫЙ Я ПИСАЛ БЕЗ ПОДСКАЗОК, ДРУГОЙ СПОСОБ (ЗАДАНИЯ 1-3)
+
+// let adds = document.querySelectorAll('.promo__adv img'),
+//     genre = document.querySelectorAll('.promo__genre'),
+//     bgPromo = document.querySelector('.promo__bg'),
+//     list = document.querySelectorAll('.promo__interactive-item');
+
+//     adds.forEach(item => {
+//         item.remove();
+//     });
+
+//     let divGenre = document.createElement('div');
+//     divGenre.classList.add('promo__genre');
+//     divGenre.textContent = 'ДРАМА';
+    
+//     genre.forEach(item => {
+//         item.replaceWith(divGenre);
+//     });
+    
+// bgPromo.style.cssText = 'background: url("img/bg.jpg")';
+
+// list.forEach(item => {
+//     item.remove();
+// });
+
+// movieDB.movies.sort();
+
+let adds = document.querySelectorAll('.promo__adv img'),
+    bgPromo = document.querySelector('.promo__bg'),
+    genre = bgPromo.querySelector('.promo__genre'),
+    Movielist = document.querySelector('.promo__interactive-list');
+    
+    adds.forEach(item => {
+                item.remove();
+            });
+
+
+genre.textContent = 'ДРАМА';
+
+bgPromo.style.backgroundImage = 'url("img/bg.jpg")';
+
+Movielist.innerHTML = '';
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    Movielist.innerHTML += `
+    <li class="promo__interactive-item">${i + 1}. ${film}
+        <div class="delete"></div>
+    </li>
+    `;
+});
+
+
+
+
+
 
